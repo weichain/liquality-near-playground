60c8adcb01c093f6f64aa9d4f258ad68ab39a73d
kraikov <raykov@weichain.tech>
2021-02-18 10:10:32 +0200

    Add secret lenght check and remove logs.

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,10 +1,9 @@
-import { storage, logging, u256, context, math, ContractPromiseBatch } from "near-sdk-as";
+import { storage, u256, context, math, ContractPromiseBatch } from "near-sdk-as";
 import { Swap } from "./model";
 
 export function init(secretHash: Uint8Array, expiration: u64, buyer: string): void {
   assert(!storage.hasKey("swap"), "ALREADY_INITIALIZED.");
   assert(secretHash.length == 32, "INVALID_SECRET_HASH");
-  logging.log("Init at: " + expiration.toString() + " for Buyer: " + buyer);
   storage.set("swap", { secretHash, expiration, buyer, seller: context.sender } as Swap);
 }
 
@@ -14,7 +13,7 @@ export function getSwap(): Swap {
 
 export function claim(secret: Uint8Array): void {
   const swapInfo = getSwap();
-  logging.log("Expiration: " + swapInfo.expiration.toString() + " Block Timestamp: " + context.blockTimestamp.toString());
+  assert(secret.length == 32, "INVALID_SECRET");
   assert(u256.fromUint8ArrayLE(math.sha256(secret)) == u256.fromUint8ArrayLE(swapInfo.secretHash), "INVALID_SECRET");
   assert(context.blockTimestamp <= swapInfo.expiration, "SWAP_EXPIRED");
   ContractPromiseBatch.create(context.contractName).delete_account(swapInfo.buyer);
@@ -22,7 +21,6 @@ export function claim(secret: Uint8Array): void {
 
 export function refund(): void {
   const swapInfo = getSwap();
-  logging.log("Expiration: " + swapInfo.expiration.toString() + " Block Timestamp: " + context.blockTimestamp.toString());
   assert(context.blockTimestamp > swapInfo.expiration, "SWAP_NOT_EXPIRED");
   ContractPromiseBatch.create(context.contractName).delete_account(swapInfo.seller);
 }
